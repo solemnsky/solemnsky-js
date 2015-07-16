a61d0240461542208e2691f7b1243228260b28f3
Glenn Smith <couleeapps@gmail.com>
2015-07-16 17:36:02 -0400

    CreateBox()

--- main.js
+++ main.js
@@ -33,6 +33,28 @@ var movement = {
 	down: false
 };
 
+function createBox(x, y, w, h) {
+	var fixDef = new b2FixtureDef;
+	fixDef.density = 1.0;
+	fixDef.friction = 0.5;
+	fixDef.restitution = 0.2;
+ 
+	var bodyDef = new b2BodyDef;
+
+	//create ground
+	bodyDef.type = b2Body.b2_staticBody;
+	 
+	// positions the center of the object (not upper left!)
+	bodyDef.position.x = x / world.scale;
+	bodyDef.position.y = y / world.scale;
+	 
+	fixDef.shape = new b2PolygonShape;
+	 
+	// half width, half height. eg actual height here is 1 unit
+	fixDef.shape.SetAsBox(w / world.scale, h / world.scale);
+	world.CreateBody(bodyDef).CreateFixture(fixDef);
+}
+
 function init() {
 	world = new b2World(
 		new b2Vec2(0, 10)    //gravity
@@ -47,20 +69,7 @@ function init() {
 	fixDef.restitution = 0.2;
  
 	var bodyDef = new b2BodyDef;
- 
-	//create ground
-	bodyDef.type = b2Body.b2_staticBody;
-	 
-	// positions the center of the object (not upper left!)
-	bodyDef.position.x = canvas.width / 2 / world.scale;
-	bodyDef.position.y = canvas.height / world.scale;
-	 
-	fixDef.shape = new b2PolygonShape;
-	 
-	// half width, half height. eg actual height here is 1 unit
-	fixDef.shape.SetAsBox((600 / world.scale) / 2, (10/world.scale) / 2);
-	world.CreateBody(bodyDef).CreateFixture(fixDef);
- 
+
 	//create some objects
 	bodyDef.type = b2Body.b2_dynamicBody;
 	fixDef.shape = new b2PolygonShape;
@@ -74,6 +83,8 @@ function init() {
 	world.block = world.CreateBody(bodyDef);
 	world.block.CreateFixture(fixDef);
 	world.block.SetSleepingAllowed(false);
+
+	createBox(canvas.width / 2, canvas.height, 300, 5);
  
 	//setup debug draw
 	var debugDraw = new b2DebugDraw();
