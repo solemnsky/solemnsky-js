5edf0a136facfae116310e45bc4a6c06c53da0db
Glenn Smith <couleeapps@gmail.com>
2015-07-16 19:23:36 -0400

    Holy shit comments

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-var 	 b2Vec2          = Box2D.Common.Math.b2Vec2
+//Shorthands so we don't have long names for the box2d types
+var	  b2Vec2         = Box2D.Common.Math.b2Vec2
 	, b2BodyDef      = Box2D.Dynamics.b2BodyDef
 	, b2Body         = Box2D.Dynamics.b2Body
 	, b2FixtureDef   = Box2D.Dynamics.b2FixtureDef
@@ -8,7 +9,8 @@ var 	 b2Vec2          = Box2D.Common.Math.b2Vec2
 	, b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape
 	, b2CircleShape  = Box2D.Collision.Shapes.b2CircleShape
 	, b2DebugDraw    = Box2D.Dynamics.b2DebugDraw;
- 
+
+//List of boxes with which to initialize the world 
 var boxes = [
 	{x:  90, y:  30, w: 40, h: 40, static: true, fields: {restitution: 0.7}},
 	{x: 130, y: 110, w: 40, h: 40, static: true, fields: {restitution: 0.7}},
@@ -21,8 +23,8 @@ var boxes = [
 ];
 
 //http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-window.requestAnimFrame = (function(){
-	return  window.requestAnimationFrame       || 
+window.requestAnimFrame = (function() {
+	return window.requestAnimationFrame   || 
 		window.webkitRequestAnimationFrame || 
 		window.mozRequestAnimationFrame    || 
 		window.oRequestAnimationFrame      || 
@@ -32,40 +34,61 @@ window.requestAnimFrame = (function(){
 		};
 })();
 
+//Some global variables for the DOM
 var canvas = document.getElementById("c");
 var ctx = canvas.getContext("2d");
+
+//The game world
+var world;
+//Current list of projectiles shot from the box
+var projectiles = [];
+
+//Current high score
 var hiscore = 0;
 
-var world;
+//Movement keys, if they're held down
 var movement = {
 	forward: false,
 	backward: false,
 	up: false,
 	down: false
 };
-var projectiles = [];
 
+/**
+ * Create a box in the world with the specified properties
+ * @param x The box's x center position (in pixels)
+ * @param y The box's y center position (in pixels)
+ * @param w The box's width (in pixels)
+ * @param h The box's height (in pixels)
+ * @param static If the box should be static (true for dynamic)
+ * @param fields An object containing fields for the box
+ */
 function createBox(x, y, w, h, static, fields) {
+	//Create a fixture definition for the box
 	var fixDef = new b2FixtureDef;
 	fixDef.density = 1.0;
 	fixDef.friction = 0.5;
 	fixDef.restitution = 0.6;
 
-	if (typeof fields.density !== "undefined") fixDef.density = fields.density;
-	if (typeof fields.friction !== "undefined") fixDef.friction = fields.friction;
-	if (typeof fields.restitution !== "undefined") fixDef.restitution = fields.restitution;
- 
+	//Read from the fields, if they exist
+	if (typeof fields !== "undefined") {
+		if (typeof fields.density !== "undefined") fixDef.density = fields.density;
+		if (typeof fields.friction !== "undefined") fixDef.friction = fields.friction;
+		if (typeof fields.restitution !== "undefined") fixDef.restitution = fields.restitution;
+	}
+
+	//Create the body definition
 	var bodyDef = new b2BodyDef;
 
-	//create ground
+	//Box type defined by the caller
 	bodyDef.type = (static ? b2Body.b2_staticBody : b2Body.b2_dynamicBody);
-	 
-	// positions the center of the object (not upper left!)
+	
+	//Positions the center of the object (not upper left!)
 	bodyDef.position.x = x / world.scale;
 	bodyDef.position.y = y / world.scale;
-	 
+	
 	fixDef.shape = new b2PolygonShape;
-	 
+	
 	// half width, half height. eg actual height here is 1 unit
 	fixDef.shape.SetAsBox(w / 2 / world.scale, h / 2 / world.scale);
 	box = world.CreateBody(bodyDef);
@@ -73,27 +96,36 @@ function createBox(x, y, w, h, static, fields) {
 	return box;
 }
 
+/**
+ * Initialize the game world 
+ */
 function init() {
+	//Default world gravity
 	gravity = new b2Vec2(0, 10);
+
+	//Create the world
 	world = new b2World(
 		gravity //gravity
 		, true  //allow sleep
 	);
 	world.gravity = gravity;
-	 
+	
+	//Meters -> Pixels scale
 	world.scale = 30;
 
+	//Create the player (who is a block)
 	world.block = createBox(canvas.width / 2, canvas.height / 2, 30, 30, false, {});
+	//Don't sleep the player so we can move it all the time
 	world.block.SetSleepingAllowed(false);
 
+	//Create all the boxes from the list above
 	for (var i = 0; i < boxes.length; i ++) {
 		createBox(boxes[i].x, boxes[i].y, boxes[i].w, boxes[i].h, boxes[i].static, boxes[i].fields);
 	}
 
-	// world.hitme.SetGravityScale(0);
-
+	//Create the ground
 	createBox(canvas.width / 2, canvas.height, 600, 10, true, {});
- 
+
 	//setup debug draw
 	var debugDraw = new b2DebugDraw();
 	debugDraw.SetSprite(document.getElementById("c").getContext("2d"));
@@ -104,6 +136,9 @@ function init() {
 	world.SetDebugDraw(debugDraw);
 }; // init()
 
+/**
+ * Method that is called on every update 
+ */
 function update() {
 	world.Step(
 		1 / 60   //frame-rate
@@ -112,88 +147,113 @@ function update() {
 	);
 	world.DrawDebugData();
 	world.ClearForces();
- 
+
 	requestAnimFrame(update);
 
+	//What position is our player at? Use this for the new projectiles
 	var blockPos = new b2Vec2(world.block.GetPosition().x, world.block.GetPosition().y);
 	blockPos.Multiply(world.scale);
 
+	//Modify your velocity to fly around in midair
 	var linearVelocity = world.block.GetLinearVelocity();
 	if (movement.forward) {
+		//Move our player
 		linearVelocity.Add(b2Vec2.Make(0, -10.0 / world.scale));
+		//Create a new box from our player
 		var box = createBox(blockPos.x, blockPos.y + 10, 10, 10, false, {});
+		//Add it to the list of projectiles
 		projectiles.push(box);
+		//Shoot it out from us
 		box.SetLinearVelocity(new b2Vec2(0, 1000 / world.scale));
 	}
 	if (movement.backward) {
+		//Move our player
 		linearVelocity.Add(b2Vec2.Make(0, 10.0 / world.scale));
+		//Create a new box from our player
 		var box = createBox(blockPos.x, blockPos.y - 10, 10, 10, false, {});
+		//Add it to the list of projectiles
 		projectiles.push(box);
+		//Shoot it out from us
 		box.SetLinearVelocity(new b2Vec2(0, -1000 / world.scale));
 	}
 	if (movement.left) {
+		//Move our player
 		linearVelocity.Add(b2Vec2.Make(-10.0 / world.scale, 0));
+		//Create a new box from our player
 		var box = createBox(blockPos.x + 10, blockPos.y, 10, 10, false, {});
+		//Add it to the list of projectiles
 		projectiles.push(box);
+		//Shoot it out from us
 		box.SetLinearVelocity(new b2Vec2(1000 / world.scale, 0));
 	}
 	if (movement.right) {
+		//Move our player
 		linearVelocity.Add(b2Vec2.Make(10.0 / world.scale, 0));
+		//Create a new box from our player
 		var box = createBox(blockPos.x - 10, blockPos.y, 10, 10, false, {});
+		//Add it to the list of projectiles
 		projectiles.push(box);
+		//Shoot it out from us
 		box.SetLinearVelocity(new b2Vec2(-1000 / world.scale, 0));
 	}
 	world.block.SetLinearVelocity(linearVelocity);
 
+	//Count score while garbage collecting
 	var score = 0;
-
+	//Garbage collector (backwards because faster and we can mutate it)
 	for (var i = projectiles.length - 1; i >= 0; i--) {
+		//Below the bottom of the screen? Delete it
 		if (projectiles[i].GetPosition().y * world.scale > canvas.height) {
 			world.DestroyBody(projectiles[i]);
 			projectiles.splice(i, 1);
-			i --;
 			continue;
 		}
+		//Off the side of the screen? Delete it
 		if (projectiles[i].GetPosition().x * world.scale > canvas.width) {
 			world.DestroyBody(projectiles[i]);
 			projectiles.splice(i, 1);
-			i --;
 			continue;
 		}
+		//Off the side of the screen? Delete it
 		if (projectiles[i].GetPosition().x * world.scale < 0) {
 			world.DestroyBody(projectiles[i]);
 			projectiles.splice(i, 1);
-			i --;
 			continue;
 		}
+		//Off the top of the screen? Don't delete it
 		if (projectiles[i].GetPosition().y * world.scale < 0) {
-			//Don't destroy if above the limit
+			//Don't let us get points though
 			continue;
 		}
+		//Points!
 		score ++;
-		if (!projectiles[i].IsAwake())
-			score ++;
 	}
 
+	//If this is our new high score, set it
 	if (score > hiscore)
 		hiscore = score;
 
+	//Let us know about our high score
 	document.getElementById("bodycount").innerHTML = world.GetBodyCount() + " bodies";
 	document.getElementById("score").innerHTML = "Score: " + score + " High Score: " + hiscore;
 
+	//If we've fallen off the bottom of the screen
 	if (world.block.GetPosition().y * world.scale > canvas.height) {
 		//You lose!
 		while (projectiles.length) {
 			world.DestroyBody(projectiles.pop());
 		}
+		//Reset our position back to the center
 		world.block.SetPosition(new b2Vec2(canvas.width / 2 / world.scale, canvas.height / 2 / world.scale));
 		world.block.SetLinearVelocity(new b2Vec2(0, 0));
 	}
 }; // update()
 
+//Start up the game
 init();
 requestAnimFrame(update);
 
+//Keyboard keys, just set movement variables
 Mousetrap.bind('up',    function() { movement.forward  =  true; }, 'keydown');
 Mousetrap.bind('up',    function() { movement.forward  = false; }, 'keyup');
 Mousetrap.bind('down',  function() { movement.backward =  true; }, 'keydown');
@@ -202,5 +262,3 @@ Mousetrap.bind('left',  function() { movement.left     =  true; }, 'keydown');
 Mousetrap.bind('left',  function() { movement.left     = false; }, 'keyup');
 Mousetrap.bind('right', function() { movement.right    =  true; }, 'keydown');
 Mousetrap.bind('right', function() { movement.right    = false; }, 'keyup');
-Mousetrap.bind('space', function() {
-});
