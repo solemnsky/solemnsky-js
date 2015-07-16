4219c69b3f07042a3d26fd1d89ef325709551316
Glenn Smith <couleeapps@gmail.com>
2015-07-16 17:22:11 -0400

    Global these

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+var   b2Vec2 = Box2D.Common.Math.b2Vec2
+	, b2BodyDef = Box2D.Dynamics.b2BodyDef
+	, b2Body = Box2D.Dynamics.b2Body
+	, b2FixtureDef = Box2D.Dynamics.b2FixtureDef
+	, b2Fixture = Box2D.Dynamics.b2Fixture
+	, b2World = Box2D.Dynamics.b2World
+	, b2MassData = Box2D.Collision.Shapes.b2MassData
+	, b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape
+	, b2CircleShape = Box2D.Collision.Shapes.b2CircleShape
+	, b2DebugDraw = Box2D.Dynamics.b2DebugDraw
+		;
+ 
+
 //http://paulirish.com/2011/requestanimationframe-for-smart-animating/
 window.requestAnimFrame = (function(){
 	return  window.requestAnimationFrame       || 
@@ -22,18 +35,6 @@ var movement = {
 };
 
 function init() {
-	var   b2Vec2 = Box2D.Common.Math.b2Vec2
-		, b2BodyDef = Box2D.Dynamics.b2BodyDef
-		, b2Body = Box2D.Dynamics.b2Body
-		, b2FixtureDef = Box2D.Dynamics.b2FixtureDef
-		, b2Fixture = Box2D.Dynamics.b2Fixture
-		, b2World = Box2D.Dynamics.b2World
-		, b2MassData = Box2D.Collision.Shapes.b2MassData
-		, b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape
-		, b2CircleShape = Box2D.Collision.Shapes.b2CircleShape
-		, b2DebugDraw = Box2D.Dynamics.b2DebugDraw
-			;
- 
 	world = new b2World(
 		new b2Vec2(0, 10)    //gravity
 		, false               //allow sleep
