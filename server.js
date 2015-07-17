003f3a2e2d7beaa716f064cfc8b378e65258b67a
Glenn Smith <couleeapps@gmail.com>
2015-07-17 00:53:22 -0400

    Prototypes

--- server.js
+++ server.js
@@ -2,9 +2,13 @@
 Box2D = require("./assets/box2d.min.js");
 
 //Import common game engine code
-require("./game.js");
+Game = require("./game.js");
+SolemnSky = new Game();
 
-function emitBlob() {
+//Socket server
+WebSocketServer = require("ws").Server;
+
+Game.prototype.emitBlob = function() {
     var blob = players.length;
     for (var i = 0; i < players.length; i ++) {
         blob += ';' + players[i].name + ',' + players[i].id + ',' + players[i].x + ',' + players[i].y + ',' + players[i].vx + ',' + players[i].vy;
@@ -12,43 +16,44 @@ function emitBlob() {
     return blob;
 }
 
-var wss;
-var WebSocketServer = require("ws").Server;
-function openSocket(port) {
+
+function Server() {}
+
+Server.prototype.openSocket = function(port) {
 	wss = new WebSocketServer({port: port});
 
 	wss.on("connection", function connection(ws) {
 		ws.on("message", function incoming(message) {
-			parseData(ws, message);
+			GameServer.parseData(ws, message);
 		});
 		ws.on("close", function close() {
 			deletePlayer(ws.playerId);
 		});
 
-		ws.send(emitBlob() + "\n");
+		ws.send(SolemnSky.emitBlob() + "\n");
 	});
 
-	function parseData(ws, data) {
-		var split = data.split(" ");
-		var command = split[0];
-		split.splice(0, 1);
-		data = split.join(" ");
-		console.log("Command: " + command + " data: " + data);
-		switch (command) {
-			case "NAME":
-				ws.playerId = addPlayer(320, 240, data, "#00ff00", "");
-				break;
-		}
+	setInterval(this.onTick, 1 / 20.0);
+}
+
+Server.prototype.parseData = function(ws, data) {
+	var split = data.split(" ");
+	var command = split[0];
+	split.splice(0, 1);
+	data = split.join(" ");
+	// console.log("Command: " + command + " data: " + data);
+	switch (command) {
+		case "NAME":
+			ws.playerId = addPlayer(320, 240, data, "#00ff00", "");
+			break;
 	}
 }
 
-openSocket(50042);
-
-function onTick() {
+Server.prototype.onTick = function() {
 	//Send all the clients a tick message
 	wss.clients.forEach(function each(client) {
 		try {
-			client.send(emitBlob() + "\n");
+			client.send(SolemnSky.emitBlob() + "\n");
 		} catch (e) {
 			//They've disconnected
 
@@ -56,4 +61,5 @@ function onTick() {
 	});
 }
 
-setInterval(onTick, 1 / 20.0);
+GameServer = new Server();
+GameServer.openSocket(50042);
