2e984a36559e293d24431d2877bd7a0aef300584
Chris Gadzinski <zenmags@gmail.com>
2015-07-19 02:07:05 +0000

    yawn

--- js/offline.js
+++ js/offline.js
@@ -1,92 +1,37 @@
 /*                  ******** offline.js ********                   //
 \\ This file defines a simple offline client. It aims to be as     \\
-// simple as possible, delegating all logic to the game engine.    // 
+// simple as possible, delegating all logic to game libraries.     // 
 \\ It exists for testing and demonstration purposes.               \\ 
 //                  ******** offline.js ********                   */
 
-/**** {{{ constants, helper functions ****/
-// global DOM values
-var canvas = document.getElementById("c");
-var ctx = canvas.getContext("2d");
+canvas = document.getElementById("c")
+ctx = canvas.getContext("2d")
+
+SolemnSky = new Game();
+SolemnSky.setFPS(60);
+SolemnSky.init();
 
 // smart request animation frame
-window.requestAnimFrame = (function() {
+requestAnimFrame = (function() {
 	return window.requestAnimationFrame   || 
 		window.webkitRequestAnimationFrame || 
 		window.mozRequestAnimationFrame    || 
 		window.oRequestAnimationFrame      || 
 		window.msRequestAnimationFrame     || 
-		function(callback, /* DOMElement */ element){
+		function(callback, element){
 			window.setTimeout(callback, SolemnSky.tickTimeMs);
 		};
 })();
-/**** }}} constants, helper functions ****/
 
-/**** {{{ game state ****/
-SolemnSky = new Game();
-SolemnSky.setFPS(60);
-SolemnSky.init();
-SolemnSky.addUpdateCallback(render);
 requestAnimFrame(update);
 
 function update() {
 	SolemnSky.update()
+	render(canvas, ctx)
 	requestAnimFrame(update)
 }
-/**** }}} game state ****/
 
-/**** {{{ rendering functions ****/
-function renderBox(body, width, height) {
-	//Reset the transform of the context
-	ctx.resetTransform();
-
-	//Box position
-	var playerX = body.GetPosition().x * SolemnSky.scale;
-	var playerY = body.GetPosition().y * SolemnSky.scale;
-
-	//Transform the context to render this in position
-	ctx.translate(playerX, playerY);
-	ctx.rotate(body.GetAngle());
-	
-	//TODO: Use the shape's path
-	ctx.fillRect(-width / 2, -height / 2, width, height);
-	ctx.strokeRect(-width / 2, -height / 2, width, height);
-}
-
-function render() {
-	//Clear the display before rendering
-	ctx.resetTransform();
-	ctx.fillStyle = "#ffffff";
-	ctx.fillRect(0, 0, windowSize.width, windowSize.height);
-
-	//Render player
-	SolemnSky.players.forEach(function each(player) {
-		ctx.fillStyle = "#bbffbb";
-		ctx.strokeStyle = "#77ff77";
-
-		renderBox(player.block, 30, 30);
-
-		var playerX = player.block.GetPosition().x * this.scale;
-		var playerY = player.block.GetPosition().y * this.scale;
-
-		ctx.resetTransform();
-		ctx.fillStyle = "#000000";
-		ctx.textAlign = "center";
-		ctx.textBaseline = "middle";
-		ctx.fillText(player.name, playerX, playerY);
-	}, SolemnSky);
-	SolemnSky.map.forEach(function each(box) {
-		var data = box.GetUserData();
-
-		ctx.fillStyle = "#" + tinycolor("hsv(" + (100 * box.life / data.fields.life) + ", 30, 100)").toHex();
-		ctx.strokeStyle = "#" + tinycolor("hsv(" + (100 * box.life / data.fields.life)+ ", 50, 100)").toHex();
-
-		renderBox(box, data.w, data.h);
-	}, SolemnSky);
-} 
-/**** }}} rendering functions ****/
-
-/**** {{{ key bindings ****/
+// key bindings
 Mousetrap.bind('up', function() { 
 		SolemnSky.findPlayerById(myid).movement.forward = true;
 	}, 'keydown');
