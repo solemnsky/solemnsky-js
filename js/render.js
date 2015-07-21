3ea38fa7ca98c7b5c8749bd4576a722a3dd28ac3
Glenn Smith <couleeapps@gmail.com>
2015-07-21 15:48:47 -0400

    Map rendering

--- js/render.js
+++ js/render.js
@@ -33,24 +33,60 @@ function smartResize() {
 /**** }}} smartResize() ****/
 
 /**** {{{ renderGame() ****/
+
+/**** {{{ drawing various types of entities ****/
 function drawPlayer(player) {
+	//Only create a sprite for this player if we haven't already
 	if (typeof(player.sprite) === "undefined") {
+		//TODO: Temporary bunny texture until we can get better assets
 		var texture = PIXI.Texture.fromImage('http://pixijs.github.io/examples/_assets/basics/bunny.png');
 		var bunny = new PIXI.Sprite(texture);
+		//Box2d objects are anchored in the center
 		bunny.anchor.x = 0.5;
 		bunny.anchor.y = 0.5;
+		//Assign it so we don't create more
 		player.sprite = bunny;
+		//TODO: Temp scaling
 		player.sprite.scale = new PIXI.Point(4, 4)
 		stage.addChild(bunny);
 	}
+	//Update position of the sprite to the player's position
 	var position = player.block.GetPosition()
 	player.sprite.position.x = position.x * SolemnSky.scale
 	player.sprite.position.y = position.y * SolemnSky.scale
 }
 
+function drawBox(box) {
+	//Only create a sprite for this box if we haven't already
+	if (typeof(box.sprite) === "undefined") {
+		//TODO: Temporary bunny texture until we can get better assets
+		var texture = PIXI.Texture.fromImage('http://pixijs.github.io/examples/_assets/basics/bunny.png');
+		var bunny = new PIXI.Sprite(texture);
+		//Box2d objects are anchored in the center
+		bunny.anchor.x = 0.5;
+		bunny.anchor.y = 0.5;
+		//Assign it so we don't create more
+		box.sprite = bunny;
+		//TODO: Temp scaling
+		box.sprite.scale = new PIXI.Point(2, 2)
+		stage.addChild(bunny);
+	}
+	//Update position of the sprite to the box's position
+	var position = box.GetPosition()
+	box.sprite.position.x = position.x * SolemnSky.scale
+	box.sprite.position.y = position.y * SolemnSky.scale
+}
+/**** }}} drawing various types of entities ****/
+
 function renderGame() {
+	//Render players
 	SolemnSky.players.forEach(function(player) {
 		drawPlayer(player);
+	});
+
+	//Render the map
+	SolemnSky.map.forEach(function(box) {
+		drawBox(box);
 	});
 }
 /**** }}} renderGame() ****/
