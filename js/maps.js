07002b8b40c5ab2a3900bdf5b2950b66c432a35d
Glenn Smith <couleeapps@gmail.com>
2015-07-21 16:08:19 -0400

    This was moved too

--- js/maps.js
+++ js/maps.js
@@ -2,7 +2,7 @@
 \\ This file defines a set of maps.                                \\
 //                  ******** maps.js ********                      */
 maps = {
-	bloxMap:  [
+	bloxMap: [
 		{x: 320, y: 480, w: 600, h: 10, static: true, fields: {life: 1e300}},
 		{x:  90, y:  30, w: 40, h: 40, static: true, fields: {restitution: 0.7, life: 10000}},
 		{x: 130, y: 110, w: 40, h: 40, static: true, fields: {restitution: 0.7, life: 10000}},
@@ -13,4 +13,8 @@ maps = {
 		{x: 430, y: 270, w: 40, h: 40, static: true, fields: {restitution: 0.7, life: 10000}},
 		{x: 570, y: 300, w: 40, h: 40, static: true, fields: {restitution: 0.7, life: 10000}}
 	]
+}
+
+if (typeof(module) !== "undefined") {
+	module.exports = maps;
 }
--- server.js
+++ server.js
@@ -19,17 +19,7 @@ SolemnSky = require("./js/gamebase.js");
 /**** }}} dependencies ****/
 
 /**** {{{ arbitrary box array (static game environment) ****/
-var boxes = [
-	{x: 320, y: 480, w: 600, h: 10, static: true, fields: {life: 1e300}},
-	{x:  90, y:  30, w: 40, h: 40, static: true, fields: {restitution: 0.7, life: 10000}},
-	{x: 130, y: 110, w: 40, h: 40, static: true, fields: {restitution: 0.7, life: 10000}},
-	{x: 470, y: 230, w: 40, h: 40, static: true, fields: {restitution: 0.7, life: 10000}},
-	{x: 210, y: 130, w: 40, h: 40, static: true, fields: {restitution: 0.7, life: 10000}},
-	{x: 350, y:  30, w: 40, h: 40, static: true, fields: {restitution: 0.7, life: 10000}},
-	{x: 390, y: 140, w: 40, h: 40, static: true, fields: {restitution: 0.7, life: 10000}},
-	{x: 430, y: 270, w: 40, h: 40, static: true, fields: {restitution: 0.7, life: 10000}},
-	{x: 570, y: 300, w: 40, h: 40, static: true, fields: {restitution: 0.7, life: 10000}}
-];
+boxes = require("./js/maps.js").bloxMap;
 /**** }}} arbitrary boxes array ****/
 
 /**** {{{ broadcastSnap: constant snapshots ****/
