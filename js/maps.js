ca255fdec6c4b8210ec811a2e0cc862a5da530c9
Glenn Smith <couleeapps@gmail.com>
2015-07-25 22:56:11 -0400

    Updated map slightly

--- js/maps.js
+++ js/maps.js
@@ -4,16 +4,16 @@
 maps = {
 	bloxMap:  [
 		// bounding blocks
-		{x: 800, y: 2, w: 1600, h: 4, static: true, fields: {}},
-		{x: 800, y: 898, w: 1600, h: 4, static: true, fields: {}},
-		{x: 5, y: 450, w: 10, h: 900, static: true, fields: {}},
-		{x: 1595, y: 450, w: 10, h: 900, static: true, fields: {}},
+		{x:  800, y:   5, w: 1600, h:  10, static: true, fields: {}},
+		{x:  800, y: 895, w: 1600, h:  10, static: true, fields: {}},
+		{x:    5, y: 450, w:   10, h: 900, static: true, fields: {}},
+		{x: 1595, y: 450, w:   10, h: 900, static: true, fields: {}},
 
-		{x:  290, y:  130, w: 40, h: 40, static: true, fields: {}},
+		{x: 290, y: 130, w: 40, h: 40, static: true, fields: {}},
 		{x: 330, y: 210, w: 40, h: 40, static: true, fields: {}},
 		{x: 670, y: 330, w: 40, h: 40, static: true, fields: {}},
 		{x: 410, y: 230, w: 40, h: 40, static: true, fields: {}},
-		{x: 550, y:  130, w: 40, h: 40, static: true, fields: {}},
+		{x: 550, y: 130, w: 40, h: 40, static: true, fields: {}},
 		{x: 590, y: 240, w: 40, h: 40, static: true, fields: {}},
 		{x: 590, y: 200, w: 20, h: 10, static: true, fields: {}},
 		{x: 490, y: 280, w: 20, h: 10, static: true, fields: {}},
