44f90740c865182223ba4fe67b8008443475d74d
Chris Gadzinski <zenmags@gmail.com>
2015-07-29 21:56:45 +0000

    browser-exports and server-exports

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,5 @@
 rm -rf js-bundle/*
-for i in js/exports/*; do 
+for i in js/browser-exports/*; do 
 	echo "building $i"
 	(browserify $i || ~/node_modules/browserify/bin/cmd.js $i) > js-bundle/$(basename $i);
 done;
