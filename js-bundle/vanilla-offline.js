cc9e9838ae2eb10922255cca8c317a2b83622a97
Chris Gadzinski <zenmags@gmail.com>
2015-07-30 19:08:28 +0000

    description -> modeId

--- js-bundle/vanilla-offline.js
+++ js-bundle/vanilla-offline.js
@@ -533,14 +533,14 @@ window.addEventListener("keyup", keyHandler(false), true)
 clientCore = require('./client-core.js')
 PIXI = require('../../assets/pixi.min.js')
 
-module.exports = function(mode, key, description) {
+module.exports = function(mode, key) {
 
 // overlay
 overlay = new PIXI.Container()
 text1 = new PIXI.Text("offline demo" , {fill: 0xFFFFFF})
 text1.position = new PIXI.Point(800, 15)
 overlay.addChild(text1)
-text2 = new PIXI.Text(description , {fill: 0xFFFFFF})
+text2 = new PIXI.Text("modeId = " + mode.modeId , {fill: 0xFFFFFF})
 text2.position = new PIXI.Point(800, 850)
 overlay.addChild(text2)
 
--- js/control/client-offline.js
+++ js/control/client-offline.js
@@ -4,14 +4,14 @@
 clientCore = require('./client-core.js')
 PIXI = require('../../assets/pixi.min.js')
 
-module.exports = function(mode, key, description) {
+module.exports = function(mode, key) {
 
 // overlay
 overlay = new PIXI.Container()
 text1 = new PIXI.Text("offline demo" , {fill: 0xFFFFFF})
 text1.position = new PIXI.Point(800, 15)
 overlay.addChild(text1)
-text2 = new PIXI.Text(description , {fill: 0xFFFFFF})
+text2 = new PIXI.Text("modeId = " + mode.modeId , {fill: 0xFFFFFF})
 text2.position = new PIXI.Point(800, 850)
 overlay.addChild(text2)
 
